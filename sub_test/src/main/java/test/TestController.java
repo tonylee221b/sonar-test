2a3c62c1488fcd0585d4146f485613c9eb8f3e1a
Hyeongseok Lee <tonyhslee@autocrypt.io>
2023-12-28 13:41:15 +0900

    first init22

--- sub_test/src/main/java/test/TestController.java
+++ sub_test/src/main/java/test/TestController.java
@@ -4,12 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
--- sub_test/src/main/java/test/TestRepository.java
+++ sub_test/src/main/java/test/TestRepository.java
@@ -3,12 +3,9 @@ package test;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TestRepository extends JpaRepository<TestEntity, Long> {
-	@Query(value = "select t.name from TestEntity t where t.name like ")
 	Optional<TestEntity> findByName(String name);
 }
